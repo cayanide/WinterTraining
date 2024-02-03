a7f0d7ca2f8598503519f20d25e1d412eefbcc74
babayan09 <63171161+babayan09@users.noreply.github.com>
2024-02-03 23:15:07 +0530

    unsigned int type

--- dataTypes.cpp
+++ dataTypes.cpp
@@ -16,6 +16,7 @@ int main(){
 
     bool TF = true;
     cout<<TF<<endl;
+    
 int size;
 
      size = sizeof(a);
---
+++ storePnNnumber.cpp
@@ -0,0 +1,15 @@
+//Storing only the postive and negative numbers
+#include<iostream>
+using namespace std;
+
+int main(){
+    unsigned int jkl;
+
+    cin>>jkl;
+    cout<<"only +ve  number : -" <<jkl<<endl;
+
+    signed int kjl;
+
+    cin>>kjl;
+    cout<<"only -ve  number : -"<< kjl<<endl;
+}
