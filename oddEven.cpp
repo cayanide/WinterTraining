43c6003be27e48941db3b79095bdfbe97a73df90
babayan09 <63171161+babayan09@users.noreply.github.com>
2024-02-03 22:37:02 +0530

    new

---
+++ oddEven.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n =0;
+    cout << "Enter your Number to check ODD or EVEN" << endl;
+    cin>>n;
+    if(n%2==0){
+        cout<<"YES , It's Even Number"<<endl;
+
+    }
+    else{
+        cout<<"NO, It's Odd Number"<<endl;
+    }
+}
