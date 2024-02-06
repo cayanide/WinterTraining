06ebf373940b0e6e1981ce38096af42cbbbdf70f
babayan09 <63171161+babayan09@users.noreply.github.com>
2024-02-06 21:25:00 +0530

    iop

---
+++ BBitwise.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int a=4;
+    int b=5;
+
+ cout<<(17>>1)<<endl;
+ cout<<(17>>2)<<endl;
+ cout<<(17>>3)<<endl;
+ cout<<(17>>4)<<endl;
+ cout<<(17>>5)<<endl;
+ cout<<(17>>6)<<endl;
+ cout<<(17>>7)<<endl;
+ cout<<(17>>8)<<endl;
+ cout<<(17>>9)<<endl;
+ cout<<(17>>10)<<endl;
+ cout<<(17<<1)<<endl;
+ cout<<(17<<2)<<endl;
+ cout<<(17<<3)<<endl;
+ cout<<(17<<4)<<endl;
+ cout<<(17<<5)<<endl;
+ cout<<(17<<6)<<endl;
+ cout<<(17<<7)<<endl;
+ cout<<(17<<8)<<endl;
+ cout<<(17<<9)<<endl;
+ cout<<(17<<10)<<endl;
+ 
+
+
+}
