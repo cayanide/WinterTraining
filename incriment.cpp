94432b5d5a2e51b1fdf78eb7af9cb5436800cd40
babayan09 <63171161+babayan09@users.noreply.github.com>
2024-02-13 15:54:35 +0530

    opo

---
+++ incriment.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int i=3;
+    
+cout<<i<<"=="<<"Printing value ";
+cout<<endl;
+cout<<i++<<"==Printing value with post increment ";
+cout<<endl;
+cout<<i<<"==Printing value after post increment ";
+cout<<endl;
+cout<<++i<<"==Printing value  pre-increment ";;
+cout<<endl;
+cout<<i<<"==Printing value with pre-increment ";
+cout<<endl;
+
+}
