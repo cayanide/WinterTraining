83ca28a12b55f90efb0f27c2a3edc82034d2aaf9
babayan09 <63171161+babayan09@users.noreply.github.com>
2024-03-03 10:56:51 +0530

    op

--- Rust/hello_world/src/main.rs
+++ Rust/hello_world/src/main.rs
@@ -1,22 +1,9 @@
 fn main() {
-    let name: String = "Sayan".to_string();
-    println!("Hello, {}!", name);
-    let y = 7;
-    let y=20;
-    println!("{}",y);
+    let mut x: i32=5;
+    let _y: i32;
+    x  = x+2; 
     
-    if y>10 {
-        println!("YES!")
-    }
-    else {
-        println!("NO!")
-    }
+    println!("Succeeces : {}",x);
+    
 
-    let grade ='C';
-     match grade{
-        'A' | 'B' => println!("Excellent grade"),
-        'C' | 'D' => println!("Good grade"),
-        'F' | 'G' => println!("Poor grade"),
-        _ => println!("Invalid grade"),
-     }
 }
