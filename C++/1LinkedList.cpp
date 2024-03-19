294b7afb553f77e971727d8acaed2b943ee88d05
babayan09 <63171161+babayan09@users.noreply.github.com>
2024-03-19 14:58:59 +0530

    Perfect Linked List
    perfectly implemented Linked List

--- C++/1LinkedList.cpp
+++ C++/1LinkedList.cpp
@@ -1,54 +1,51 @@
 #include<iostream>
 using namespace std;
 
-
 #include<algorithm>
 
 class Node {
-
-
-    public:
+public:
     int data;
     Node* prev;
     Node* next;
-
 };
 
-
+// Function to insert a node at the head of the linked list
 Node* insertAtHead(Node* head, int data) {
-
- Node* newNode = new Node();
- newNode->data = data;
- newNode->next = head;
- newNode->prev = NULL;
- if(head!= NULL) {
-     head->prev = newNode;
- }
- head = newNode;
- return head;
- 
-}
-Node* insertAtTail(Node* head , int data){
-Node* newNode = new Node();
-newNode->data = data;
-
-newNode->next = NULL;
-
-if(head == NULL) {
+    Node* temp = head;
+    Node* newNode = new Node();
+    newNode->data = data;
+    newNode->next = temp;
+    newNode->prev = NULL;
+    if(head != NULL) {
+        temp->prev = newNode;
+    }
     head = newNode;
     return head;
 }
-else {
-    Node* temp = head;
-    while(temp->next!= NULL) {
-        temp = temp->next;
+
+// Function to insert a node at the tail of the linked list
+Node* insertAtTail(Node* head , int data) {
+    Node* newNode = new Node();
+    newNode->data = data;
+    newNode->next = NULL;
+    if(head == NULL) {
+        head = newNode;
+        return head;
     }
-    temp->next = newNode;
-    newNode->prev = temp;
-    return head;
+    else {
+        Node* temp = head;
+        while(temp->next != NULL) {
+            temp = temp->next;
+        }
+        temp->next = newNode;
+        newNode->prev = temp;
+        return head;
+    }
 }
-}
-Node* deleteAtHead(Node* head){
+
+// Function to delete a node from the head of the linked list
+Node* deleteAtHead(Node* head) {
     if (head == nullptr) { 
         return nullptr; // No elements to delete
     }
@@ -64,262 +61,232 @@ Node* deleteAtHead(Node* head){
     return head;
 }
 
-Node* deleteAtTail(Node* head){
+// Function to delete a node from the tail of the linked list
+Node* deleteAtTail(Node* head) {
     Node* temp = head;
-    if(temp!= NULL) {
-        while(temp->next!= NULL) {
-            temp = temp->next;
-        }
-        temp->next = NULL;
-        delete temp;
+    if (head == nullptr) { 
+        return nullptr; // No elements to delete
+    }
+    while (temp->next!= nullptr) {
+        temp = temp->next;
+    }
+    temp->prev->next = nullptr;
+    delete temp; // Deallocate the old tail
+
+    return head;
+  
+
+
+}
+
+// Function to print the linked list
+Node* PrintList(Node* head) {
+    Node* temp = head;
+    while(temp != NULL) {
+        cout << "->" << temp->data; 
+        temp = temp->next;
     }
     return head;
 }
 
-Node* PrintList(Node* head){
-    Node* temp =head;
-    while(temp!=NULL){
-        cout<<"->"<<temp->data; 
-        temp=temp->next;
-    }
-    return head;
-}
-
-
-Node* insertAtMiddle(Node* head, int data){
+// Function to insert a node at the middle of the linked list
+Node* insertAtMiddle(Node* head, int data) {
     Node* newNode = new Node();
     newNode->data = data;
     Node* temp = head;
     int count = 0;
-    if(temp!= NULL) {
-        while(temp->next!= NULL) {
-             count++;
+    if(temp != NULL) {
+        while(temp->next != NULL) {
+            count++;
             temp = temp->next;
-           
         }
-        int middlen = count/2;
+        int middlen = count / 2;
 
         temp = head;
-        for(int i=0;i<middlen;i++) {
+        for(int i = 0; i < middlen; i++) {
             temp = temp->next;
         }
         newNode->next = temp->next;
         temp->next = newNode;
         newNode->prev = temp;
         temp->next->prev = newNode;
-        
     }
     return head;
-
 }
 
-Node* deleteAtMiddle(Node* head){
+// Function to delete a node from the middle of the linked list
+Node* deleteAtMiddle(Node* head) {
     Node* temp = head;
     int count = 0;
-    if(temp!= NULL) {
-        while(temp->next!= NULL) {
-             count++;
+    if(temp != NULL) {
+        while(temp->next != NULL) {
+            count++;
             temp = temp->next;
-           
         }
-        int middlen = count/2;
+        int middlen = count / 2;
         temp = head;
-        for(int i=0;i<middlen;i++) {
+        for(int i = 0; i < middlen; i++) {
             temp = temp->next;
         }
         temp->next = temp->next->next;
         temp->next->prev = temp;
         delete temp->next;
-
-
+    }
     return head;
-
-}
 }
 
-Node* searchForDeletion(Node* head,int data){
+// Function to search for a node for deletion
+Node* searchForDeletion(Node* head, int data) {
     Node* temp = head;
-    while(temp!= NULL) {
+    while(temp != NULL) {
         if(temp->data == data) {
-            return temp;
-        }
+            temp->prev->next=temp->next;
+            temp->next->prev=temp->prev;
+            delete temp;
+             }
         temp = temp->next;
     }
-    
     return head;
-
 }
 
-Node* searchForInsertion(Node* head,int data){
+// Function to search for a node for insertion
+Node* searchForInsertion(Node* head,int Selement, int data) {
     Node* temp = head;
-    while(temp!= NULL) {
-        if(temp->data == data) {
-            Node* tempNode= new Node();
+    while(temp != NULL) {
+        if(temp->data == Selement) {
+            Node* tempNode = new Node();
             tempNode->data = data;
             tempNode->next = temp->next;
             tempNode->prev = temp;
             temp->next = tempNode;
             temp->next->prev = tempNode;
             return head;
-
         }
-         else
-        {
-            cout<<"Error Element not found";
+        else {
+            cout << "Error: Element not found";
         }
         temp = temp->next;
-        }
-       
+    }
     return head;
-
 }
 
-bool choices(int choice, Node* head,bool flag){
-    int element;
+
+
+
+int main() {
+    int size, i = 0, choice, element,Selement;
+    bool flag = true;
+    Node* head = nullptr;
+
+    cout << "Welcome To Linked List " << endl;
+    cout << "Enter the size of the list " << endl;
+    cin >> size;
+
+    for(i = 0; i < size; i++) {
+        cout << "Enter The Element : " << i << endl;
+        int onceValue = 0;
+        cin >> onceValue;
+        head = insertAtHead(head, onceValue);
+    }
+
+    cout << endl;
+
+    cout << "Created linked list: ";
+    cout << endl;
+
+    PrintList(head);
+
+    cout << endl;
+
+    while(flag == true) {
+    cout << " *_* Enter A Choice : " << endl;  
+    cout << "1. Insert At Head" << endl;
+    cout << "2. Insert At Tail" << endl;
+    cout << "3. Delete At Head" << endl;
+    cout << "4. Delete At Tail" << endl;
+    cout << "5. Insert At Middle" << endl;
+    cout << "6. Delete At Middle" << endl;
+    cout << "7. Print List" << endl;
+    cout << "8. Search for Deletion" << endl;
+    cout << "9. Search for Insertion" << endl;
+    cout << "0. Exit" << endl;
+    cout << "The Updated Linked List is : " << endl;
+
+    cout << endl;
+
+    PrintList(head);
+
+    cout << endl;
+    cin >> choice;
+     switch (choice) {
+        case 1:
+            cout << "Running..." << endl;
+            cout << "Enter Data For Insertion At Head : " << endl;
+            cin >> element;
+            head = insertAtHead(head, element);
+            cout << endl;
+            PrintList(head);
+            cout << endl;
+            break;
+        case 2:
+            cout << "Running..." << endl;
+            cout << "Enter Data For Insertion At Tail : " << endl;
+            cin >> element;
+            head = insertAtTail(head, element);
+            cout << endl;
+            PrintList(head);
+            cout << endl;
+            break;
+        case 3:
+            cout << "Running Deletion At Head..." << endl;
+            head = deleteAtHead(head);
+            break;
+        case 4:
+            cout << "Running Deletion At Tail..." << endl;
+            head = deleteAtTail(head);
+            break;
+        case 5:
+            cout << "Running..." << endl;
+            cout << "Enter Data For Insertion At Middle : " << endl;
+            cin >> element;
+            head = insertAtMiddle(head, element);
+            break;
+        case 6:
+            cout << "Running Deletion At Middle..." << endl;
+            cout << "Delete At Middle : " << endl;
+            head = deleteAtMiddle(head);
+            cout<<"Deleted";
+            cout<<PrintList(head);
+            break;
+        case 7:
+            cout << "Printing Linked List..." << endl;
+            head = PrintList(head);
+            break;
+        case 8:
+            cout << "Running..." << endl;
+            cout << "Enter Data For Searching for Deletion : " << endl;
+            cin >> element;
+            head = searchForDeletion(head, element);
+            break;
+        case 9:
+            cout << "Running..." << endl;
+            cout << "Enter Element For Searching : " << endl;
+            
+            cin>>Selement;
+            cout<<"Enter Data For Insertion : " << endl;
+            cin >> element;
+            head = searchForInsertion(head, Selement,element);
+            break;
+        case 0:
+            flag = false;
+            break;
+        default:
+            cout << "Invalid Choice" << endl;
+    }
+    }
     
-    
-switch (choice) {
- 
-    case 1:
-     cout<<"Running..."<<endl;
-     cout<<" Enter Data For Insertion At Head : "<< endl;
-     cin>>element;
-     head=insertAtHead(head,element);
-     PrintList(head);
-     break;
-    case 2:
-     cout<<"Running..."<<endl;
-     cout<<" Enter Data For Insertion At Tail : "<< endl;
-     cin>>element;
-     head=insertAtTail(head,element);
-     PrintList(head);
-     break;
-    case 3:
-     cout<<"Running..."<<endl;
-     head=deleteAtHead(head);
-      break;
-    case 4:
-     cout<<"Running..."<<endl;
-     head=deleteAtTail(head);
-      break;
-     case 5:
-     cout<<"Running..."<<endl;
-     cout<<" Enter Data For Insertion At Middle : "<< endl;
-     cin>>element;
-     head=insertAtMiddle(head,element);
-      break;
-    case 6:
-     cout<<"Running..."<<endl;
-     cout<<"  Delete At Middle : "<< endl;
-     head= deleteAtMiddle(head);
-      break;
-    case 7:
-     cout<<"Running..."<<endl;
-     head=PrintList(head);
-      break;
-    case 8:
-     cout<<"Running..."<<endl;
-     cout<<" Enter Data For Searching for Deletion : "<< endl;
-     cin>>element;
-     head=searchForDeletion(head,element);
-      break;
-     case 9:
-     cout<<"Running..."<<endl;
-     cout<<" Enter Data For Searching for Insertion : "<< endl;
-     cin>>element;
-     head=searchForInsertion(head,element);
-        break;
-
-    case 0:
-     flag=false;
-     break;
-     default:
-     cout<<"Invalid Choice"<<endl;
-
-     
-}
-return flag;
-}
-
-bool ChoicesPrinter(bool flag, int choice,Node* head){
-    
-
-
-cout<<"              "<<endl;
-
-cout<<"Enter A Choice : "<<endl; 
-
-cout<<"Enter 1 for Insertion At Head : "<< endl;
-
-cout<<"Enter 2 for Insertion At Tail : "<< endl;
-
-cout<<"Enter 3 for Deletion At Head : "<< endl;
-
-cout<<"Enter 4 for Deletion At Tail : "<< endl;
-
-cout<<"Enter 5 for Insertion At Middle : "<<endl;
-
-cout<<"Enter 6 for Deletetion At Middle : "<< endl;
-
-cout<<"Enter 7 for Printing the List : "<< endl;
-
-cout<<"Enter 8 for Searching for Deletion : "<< endl;
-
-cout<<"Enter 9 for Searching for Insertion : "<< endl;
-
-cout<<"The Updated Linked List is :"<<endl;
-
-cout<<endl;
-
-PrintList(head);
-
-cout<<"              "<<endl;
-
-
-
-cout<<"Enter 0 for Exit : "<< endl;
-
-
-while(flag==true){
-cin>>choice;
-flag=choices( choice,head,flag);
-}
-}
-
-
-int main(){
-    
-int size,i=0,choice,element;
-
-bool flag=true;
-
-Node* head = nullptr;
-/* ***********************************************************************************************/
-
-cout<<"Welcome To Linked List "<<endl;
-
-cout<<"Enter the size of the list "<<endl;
-
-cin>>size;
-
-
-for(i=0;i<size;i++)
-{
-    cout<<"Enter The Element : "<<i<<endl;
-    int onceValue=0;
-    cin>>onceValue;
-    head= insertAtHead(head,onceValue);
-   
-}
-cout << "Created linked list: ";
-PrintList(head);
-
-
-while(flag==true){
-    flag=ChoicesPrinter(flag,choice,head);
-    cout<<"--------------------------------"<<endl;
-}
-
-}
-
+        cout << "--------------------------------" << endl;
+        cout<<endl;
+        return 0;
+    }
 
