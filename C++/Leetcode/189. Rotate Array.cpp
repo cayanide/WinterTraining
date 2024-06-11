4d4d3a4bc947c07e341c0f37a673a801443a84d8
Sayan Chatterjee <sayanc354@gmail.com>
2024-06-11 15:09:55 +0530

    Update 189. Rotate Array.cpp

--- C++/Leetcode/189. Rotate Array.cpp
+++ C++/Leetcode/189. Rotate Array.cpp
@@ -1,20 +1,17 @@
 class Solution {
 public:
-    int removeDuplicates(vector<int>& nums) {
-    if (nums.empty()) {
-        return 0;
-    }
+    void rotate(vector<int>& nums, int k) {
 
-    int slow = 0;
-    for (int fast = 1; fast < nums.size(); ++fast) {
-        if (nums[fast] != nums[slow]) {
+        int n = nums.size();
+              k = k % n;
 
-            ++slow;
-            nums[slow] = nums[fast];
-        }
-    }
 
-    return slow + 1;
+              reverse(nums.begin(), nums.end());
 
+
+              reverse(nums.begin(), nums.begin() + k);
+
+
+              reverse(nums.begin() + k, nums.end());
     }
 };
