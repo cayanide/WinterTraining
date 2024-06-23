f415b0ca1ac49e0d5c720473395581881805a733
Sayan Chatterjee <63171161+cayanide@users.noreply.github.com>
2024-06-23 21:35:52 +0530

    4sum

--- C++/Leetcode/18. 4Sum.cpp
+++ C++/Leetcode/18. 4Sum.cpp
@@ -1,55 +1,27 @@
 class Solution {
 public:
-  vector<vector<int>> fourSum(vector<int>& nums, int target) {
-    int n = nums.size();
-    sort(nums.begin(), nums.end());
-
-    vector<vector<int>> quadruplets;
-    set<vector<int>> uniqueQuads;
-
-    for (int i = 0; i < n - 3; i++) {
-      if (i > 0 && nums[i] == nums[i - 1]) continue;
-
-      for (int j = i + 1; j < n - 2; j++) {
-        if (j > i + 1 && nums[j] == nums[j - 1]) continue;
-
-        int remaining = target - nums[i] - nums[j];
-
-        if (remaining == 0) {
-          if (j < n - 1 && (nums[j + 1] == 0 || nums[j + 1] == remaining)) {
-            vector<int> quad = {nums[i], nums[j], nums[j + 1], nums[j + 2]};
-            if (uniqueQuads.insert(quad).second) {
-              quadruplets.push_back(quad);
-            }
-          }
-        } else {
-          pair<int, int> indices = twoSumPositive(nums, remaining, j + 1);
-          if (indices.first != -1 && indices.second != -1) {
-            vector<int> quad = {nums[i], nums[j], nums[indices.first], nums[indices.second]};
-            if (uniqueQuads.insert(quad).second) {
-              quadruplets.push_back(quad);
-            }
-          }
+    vector<vector<int>> fourSum(vector<int>& nums, int target) {
+    int n =nums.size();
+    set<vector<int>> st;
+    for(int i =0; i<n;i++){
+      for(int j =i+1;j<n;j++){
+        for(int k =j+1;k<n;k++){
+        for(int l=k+1;l<n;l++){
+          long long sum =nums[i]+nums[j];
+          sum +=nums[k];
+          sum +=nums[l];
+          if(sum ==target){
+            vector<int> temp = {nums[i],nums[j],nums[k],nums[l]};
+            sort(temp.begin(),temp.end());
+            st.insert(temp);
         }
       }
     }
-
-    return quadruplets;
-  }
-
-private:
-  pair<int, int> twoSumPositive(const vector<int>& nums, int target, int left) {
-    int right = nums.size() - 1;
-    while (left < right) {
-      int sum = nums[left] + nums[right];
-      if (sum == target) {
-        return {left, right};
-      } else if (sum < target || (nums[left] == 0 && target == nums[right])) {
-        left++;
-      } else {
-        right--;
       }
-    }
-    return {-1, -1};
+
   }
+  vector<vector<int>> ans(st.begin(),st.end());
+  return ans;
+  }
+    
 };
