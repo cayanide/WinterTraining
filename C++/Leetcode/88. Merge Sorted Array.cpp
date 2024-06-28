899192c38ee53f06849c9f37c942fc1b18bf66f8
Sayan Chatterjee <sayanc354@gmail.com>
2024-06-28 13:32:38 +0530

    better space complexity

--- C++/Leetcode/88. Merge Sorted Array.cpp
+++ C++/Leetcode/88. Merge Sorted Array.cpp
@@ -1,23 +1,29 @@
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-         int p1 = m - 1;
-    int p2 = n - 1;
-    int index = m + n - 1;
+
+  int i = m - 1;
+  int j = n - 1;
+  int k = m + n - 1;
 
 
-    while (p1 >= 0 && p2 >= 0) {
-        if (nums1[p1] > nums2[p2]) {
-            nums1[index--] = nums1[p1--];
-        } else {
-            nums1[index--] = nums2[p2--];
-        }
+  while (i >= 0 && j >= 0) {
+
+    if (nums1[i] >= nums2[j]) {
+      nums1[k] = nums1[i];
+      i--;
+    } else {
+      nums1[k] = nums2[j];
+      j--;
     }
+    k--;
+  }
 
 
-    while (p2 >= 0) {
-        nums1[index--] = nums2[p2--];
-    }
-
+  while (j >= 0) {
+    nums1[k] = nums2[j];
+    j--;
+    k--;
+  }
     }
 };
