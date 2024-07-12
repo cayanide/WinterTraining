90b744cf30e70e2a5431ced2f45adbec884b4b7a
Sayan Chatterjee <sayanc354@gmail.com>
2024-07-12 13:44:48 +0530

    more ques

---
+++ C++/Leetcode/19. Remove Nth Node From End of List.cpp
@@ -0,0 +1,39 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* removeNthFromEnd(ListNode* head, int n) {
+        ListNode* dummy = new ListNode(0);
+        dummy->next = head;
+        ListNode* first = dummy;
+        ListNode* second = dummy;
+
+
+        for (int i = 1; i <= n + 1; ++i) {
+            second = second->next;
+        }
+
+
+        while (second != nullptr) {
+            first = first->next;
+            second = second->next;
+        }
+
+
+        first->next = first->next->next;
+
+
+        ListNode* newHead = dummy->next;
+        delete dummy;
+
+        return newHead;
+    }
+};
---
+++ C++/Leetcode/79. Word Search.cpp
@@ -0,0 +1,58 @@
+#include <vector>
+#include <string>
+
+using namespace std;
+
+class Solution {
+public:
+    bool exist(vector<vector<char>>& board, string word) {
+        if (board.empty() || board[0].empty() || word.empty()) return false;
+
+        int m = board.size();
+        int n = board[0].size();
+
+        vector<vector<bool>> visited(m, vector<bool>(n, false));
+
+        // Define direction vectors: up, down, left, right
+        vector<pair<int, int>> directions = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+
+        // Helper function to perform backtracking
+        auto backtrack = [&](int r, int c, int index) -> bool {
+            // Base case: If all characters in the word are matched
+            if (index == word.length()) return true;
+
+            // Check boundaries and character match
+            if (r < 0 || r >= m || c < 0 || c >= n || visited[r][c] || board[r][c] != word[index]) {
+                return false;
+            }
+
+            // Mark current cell as visited
+            visited[r][c] = true;
+
+            // Explore all 4 directions
+            for (auto& dir : directions) {
+                int nr = r + dir.first;
+                int nc = c + dir.second;
+                if (backtrack(nr, nc, index + 1)) {
+                    return true;
+                }
+            }
+
+            // Backtrack: Unmark the cell
+            visited[r][c] = false;
+
+            return false;
+        };
+
+        // Try starting from each cell in the board
+        for (int i = 0; i < m; ++i) {
+            for (int j = 0; j < n; ++j) {
+                if (backtrack(i, j, 0)) {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+};
