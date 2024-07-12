97828e8e7d6f3c576099623cb41748c6fa25d09d
Sayan Chatterjee <sayanc354@gmail.com>
2024-07-12 13:46:29 +0530

    Update 79. Word Search.cpp

--- C++/Leetcode/79. Word Search.cpp
+++ C++/Leetcode/79. Word Search.cpp
@@ -16,42 +16,43 @@ public:
         // Define direction vectors: up, down, left, right
         vector<pair<int, int>> directions = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
 
-        // Helper function to perform backtracking
-        auto backtrack = [&](int r, int c, int index) -> bool {
-            // Base case: If all characters in the word are matched
-            if (index == word.length()) return true;
-
-            // Check boundaries and character match
-            if (r < 0 || r >= m || c < 0 || c >= n || visited[r][c] || board[r][c] != word[index]) {
-                return false;
-            }
-
-            // Mark current cell as visited
-            visited[r][c] = true;
-
-            // Explore all 4 directions
-            for (auto& dir : directions) {
-                int nr = r + dir.first;
-                int nc = c + dir.second;
-                if (backtrack(nr, nc, index + 1)) {
-                    return true;
-                }
-            }
-
-            // Backtrack: Unmark the cell
-            visited[r][c] = false;
-
-            return false;
-        };
-
         // Try starting from each cell in the board
         for (int i = 0; i < m; ++i) {
             for (int j = 0; j < n; ++j) {
-                if (backtrack(i, j, 0)) {
+                if (backtrack(board, visited, word, directions, i, j, 0)) {
                     return true;
                 }
             }
         }
+
+        return false;
+    }
+
+private:
+    bool backtrack(vector<vector<char>>& board, vector<vector<bool>>& visited, const string& word,
+                   vector<pair<int, int>>& directions, int r, int c, int index) {
+        // Base case: If all characters in the word are matched
+        if (index == word.length()) return true;
+
+        // Check boundaries and character match
+        if (r < 0 || r >= board.size() || c < 0 || c >= board[0].size() || visited[r][c] || board[r][c] != word[index]) {
+            return false;
+        }
+
+        // Mark current cell as visited
+        visited[r][c] = true;
+
+        // Explore all 4 directions
+        for (auto& dir : directions) {
+            int nr = r + dir.first;
+            int nc = c + dir.second;
+            if (backtrack(board, visited, word, directions, nr, nc, index + 1)) {
+                return true;
+            }
+        }
+
+        // Backtrack: Unmark the cell
+        visited[r][c] = false;
 
         return false;
     }
