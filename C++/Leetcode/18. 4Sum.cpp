bcfd466785160f1f3c3c29d526bcdaced073b25d
Sayan Chatterjee <sayanc354@gmail.com>
2024-07-12 13:32:43 +0530

    linked list

--- C++/Leetcode/18. 4Sum.cpp
+++ C++/Leetcode/18. 4Sum.cpp
@@ -1,55 +1,43 @@
 class Solution {
 public:
-  vector<vector<int>> fourSum(vector<int>& nums, int target) {
-    int n = nums.size();
-    sort(nums.begin(), nums.end());
+    vector<vector<int>> fourSum(vector<int>& nums, int target) {
+               vector<vector<int>> result;
+        int n = nums.size();
+        sort(nums.begin(), nums.end());
 
-    vector<vector<int>> quadruplets;
-    set<vector<int>> uniqueQuads;
+        for (int i = 0; i < n - 3; ++i) {
 
-    for (int i = 0; i < n - 3; i++) {
-      if (i > 0 && nums[i] == nums[i - 1]) continue;
+            if (i > 0 && nums[i] == nums[i - 1]) continue;
 
-      for (int j = i + 1; j < n - 2; j++) {
-        if (j > i + 1 && nums[j] == nums[j - 1]) continue;
+            for (int j = i + 1; j < n - 2; ++j) {
 
-        int remaining = target - nums[i] - nums[j];
+                if (j > i + 1 && nums[j] == nums[j - 1]) continue;
 
-        if (remaining == 0) {
-          if (j < n - 1 && (nums[j + 1] == 0 || nums[j + 1] == remaining)) {
-            vector<int> quad = {nums[i], nums[j], nums[j + 1], nums[j + 2]};
-            if (uniqueQuads.insert(quad).second) {
-              quadruplets.push_back(quad);
+                int left = j + 1;
+                int right = n - 1;
+
+                while (left < right) {
+                    long long currentSum = (long long)nums[i] + nums[j] + nums[left] + nums[right];
+
+                    if (currentSum == target) {
+                        result.push_back({nums[i], nums[j], nums[left], nums[right]});
+
+
+                        while (left < right && nums[left] == nums[left + 1]) left++;
+
+                        while (left < right && nums[right] == nums[right - 1]) right--;
+
+                        left++;
+                        right--;
+                    } else if (currentSum < target) {
+                        left++;
+                    } else {
+                        right--;
+                    }
+                }
             }
-          }
-        } else {
-          pair<int, int> indices = twoSumPositive(nums, remaining, j + 1);
-          if (indices.first != -1 && indices.second != -1) {
-            vector<int> quad = {nums[i], nums[j], nums[indices.first], nums[indices.second]};
-            if (uniqueQuads.insert(quad).second) {
-              quadruplets.push_back(quad);
-            }
-          }
         }
-      }
-    }
 
-    return quadruplets;
-  }
-
-private:
-  pair<int, int> twoSumPositive(const vector<int>& nums, int target, int left) {
-    int right = nums.size() - 1;
-    while (left < right) {
-      int sum = nums[left] + nums[right];
-      if (sum == target) {
-        return {left, right};
-      } else if (sum < target || (nums[left] == 0 && target == nums[right])) {
-        left++;
-      } else {
-        right--;
-      }
+        return result;
     }
-    return {-1, -1};
-  }
 };
