57e5833e889a800bef0b393b96d46ca7d9b56501
Sayan Chatterjee <sayanc354@gmail.com>
2024-07-12 13:16:58 +0530

    Update 206. Reverse Linked List.cpp

---
+++ C++/Leetcode/206. Reverse Linked List.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+
+        if (head == nullptr || head->next == nullptr) {
+            return head;
+        }
+
+
+        ListNode* reversedListHead = reverseList(head->next);
+
+
+        head->next->next = head;
+        head->next = nullptr;
+
+        return reversedListHead;
+    }
+};
